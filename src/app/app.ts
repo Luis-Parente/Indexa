7e0b79c2244aa7e8253b0168dd936e49511c610b
Luís Parente <parente.lgsp@outlook.com>
2025-11-11 22:21:50 -0300

    finalizada configuração da barra de pesquisa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {Container} from './componentes/container/container';
 import {Cabecalho} from './componentes/cabecalho/cabecalho';
 import {Separador} from './componentes/separador/separador';
@@ -12,10 +12,11 @@ interface Dados_Contato {
 }
 
 import agenda from './agenda.json'
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Container, Cabecalho, Separador, Contato],
+  imports: [RouterOutlet, Container, Cabecalho, Separador, Contato, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -23,9 +24,24 @@ export class App {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
   contatos: Dados_Contato[] = agenda;
 
-  filtrarContatosPorLetraInicial(letra: string) : Dados_Contato[] {
+  filtroPorTexto: string = "";
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPorTexto(): Dados_Contato[] {
+    if (!this.filtroPorTexto.length) {
+      return this.contatos;
+    }
     return this.contatos.filter(contato => {
-      return contato.nome.toLowerCase().startsWith(letra);
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.filtroPorTexto.toLowerCase());
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Dados_Contato[] {
+    return this.filtrarContatosPorTexto().filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(letra);
     })
   }
 }
